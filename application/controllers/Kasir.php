98242f3b785f5e582c79e269d1423eedc9e02cef
zalramli <github.little@gmail.com>
2019-06-16 12:02:07 +0700

    --

--- application/controllers/Kasir.php
+++ application/controllers/Kasir.php
@@ -34,7 +34,8 @@ class Kasir extends CI_Controller
             'name' => $this->input->post('name'),
             'price' => $this->input->post('price'),
             'foto' => $this->input->post('foto'),
-            'qty' => $this->input->post('qty')
+            'qty' => $this->input->post('qty'),
+            'pelanggan' => "kosong"
         );
         $this->cart->insert($data_produk);
         redirect('kasir');
@@ -82,9 +83,15 @@ class Kasir extends CI_Controller
     {
         $sql = $this->db->query("SELECT COUNT(*) as counts FROM tbl_pemesanan WHERE status='belum'");
         $data['jumlah'] = $sql->result();
+        $cart = $this->cart->contents();
+        foreach ($cart as $item) {
+            $id = $item['pelanggan'];
+        }
+        $sql2 = $this->db->query("SELECT * FROM  tbl_pelanggan WHERE id_pelanggan='$id'")->result_array();
         $data['pelanggan'] = $this->m_kasir->buat_kodePelanggan();
         $data['transaksi'] = $this->m_kasir->buat_kodeTransaksi();
         $data['kategori'] = $this->m_kasir->get_kategori_all();
+        $data['data_pelanggan'] = $sql2;
         $this->load->view('kasir/check_out', $data);
     }
     public function proses_transaksi()
@@ -158,64 +165,73 @@ class Kasir extends CI_Controller
     }
     public function prosesPemesanan()
     {
+        $this->cart->destroy();
         $id = $this->input->post('id_pemesanan');
-        $id_pegawai = $this->input->post('id_pegawai');
-        $id_transaksi = $this->m_kasir->buat_kodeTransaksi();
+        // $id_pegawai = $this->input->post('id_pegawai');
+        // $id_transaksi = $this->m_kasir->buat_kodeTransaksi();
         $sql = $this->db->query("SELECT * FROM tbl_pemesanandetail JOIN tbl_barang ON tbl_pemesanandetail.id_barang = tbl_barang.id_barang JOIN tbl_kategori ON tbl_barang.id_kategori=tbl_kategori.id_kategori JOIN tbl_merk ON tbl_barang.id_merk=tbl_merk.id_merk WHERE id_pemesanan='$id'");
+        $sql2 = $this->db->query("SELECT * FROM tbl_pemesanan JOIN tbl_pelanggan ON tbl_pemesanan.id_pelanggan = tbl_pelanggan.id_pelanggan WHERE id_pemesanan='$id'")->result_array();
+        $tampung =  $sql2[0]['id_pelanggan'];
         foreach ($sql->result() as $item) {
             $data_produk = array(
                 'id' => $item->id_barang,
                 'name' => $item->nama_barang,
                 'price' => $item->harga,
                 'foto' => $item->foto,
-                'qty' => $item->jumlah
+                'qty' => $item->jumlah,
+                'pelanggan' => $tampung
             );
             $this->cart->insert($data_produk);
         }
-        $grand_total = 0;
-        if ($cart = $this->cart->contents()) {
-            foreach ($cart as $item) {
-                $grand_total = $grand_total + $item['subtotal'];
-            }
-        }
-        $data_transaksi = array(
-            'id_transaksi' => $id_transaksi,
-            'tanggal' => date('Y-m-d'),
-            'total_harga' => $grand_total,
-            'total_bayar' => 5000,
-            'total_kembalian' => 500,
-            'id_pegawai' => $id_pegawai,
-            'id_pelanggan' => $this->input->post('id_pelanggan')
-        );
-        $data_pemesanan = array(
-            'status' => 'sudah'
-        );
-        $where = array(
-            'id_pemesanan' => $id
-        );
-        $this->m_kasir->update_data($where, $data_pemesanan, 'tbl_pemesanan');
-        $this->m_kasir->input_data($data_transaksi, 'tbl_transaksi');
-        if ($cart = $this->cart->contents()) {
+        $data['transaksi'] = $this->m_kasir->buat_kodeTransaksi();
+        $data['kategori'] = $this->m_kasir->get_kategori_all();
+        $sql3 = $this->db->query("SELECT COUNT(*) as counts FROM tbl_pemesanan WHERE status='belum'");
+        $data['jumlah'] = $sql3->result();
+        redirect('kasir/check_out', $data);
+        // $grand_total = 0;
+        // if ($cart = $this->cart->contents()) {
+        //     foreach ($cart as $item) {
+        //         $grand_total = $grand_total + $item['subtotal'];
+        //     }
+        // }
+        // $data_transaksi = array(
+        //     'id_transaksi' => $id_transaksi,
+        //     'tanggal' => date('Y-m-d'),
+        //     'total_harga' => $grand_total,
+        //     'total_bayar' => 5000,
+        //     'total_kembalian' => 500,
+        //     'id_pegawai' => $id_pegawai,
+        //     'id_pelanggan' => $this->input->post('id_pelanggan')
+        // );
+        // $data_pemesanan = array(
+        //     'status' => 'sudah'
+        // );
+        // $where = array(
+        //     'id_pemesanan' => $id
+        // );
+        // $this->m_kasir->update_data($where, $data_pemesanan, 'tbl_pemesanan');
+        // $this->m_kasir->input_data($data_transaksi, 'tbl_transaksi');
+        // if ($cart = $this->cart->contents()) {
 
-            foreach ($cart as $item) {
-                $data_detail = array(
-                    'id_transaksi' => $id_transaksi,
-                    'id_barang' => $item['id'],
-                    'qty' => $item['qty'],
-                    'total_hrg' => $item['price'] * $item['qty']
-                );
-                $qq = $item['id'];
-                $q = $this->db->query("SELECT * FROM tbl_barang WHERE id_barang='$qq'")->result();
-                foreach ($q as $it) {
-                    $ids = $item['id'];
-                    $stok_awal = $it->stok_real;
-                    $stok_akhir = $stok_awal - $item['qty'];
-                    $this->db->query("UPDATE tbl_barang SET stok_real='$stok_akhir' WHERE id_barang='$ids'");
-                }
-                $this->m_kasir->input_data($data_detail, 'tbl_transaksidetail');
-            }
-        }
-        $this->cart->destroy();
-        redirect('kasir/sukses');
+        //     foreach ($cart as $item) {
+        //         $data_detail = array(
+        //             'id_transaksi' => $id_transaksi,
+        //             'id_barang' => $item['id'],
+        //             'qty' => $item['qty'],
+        //             'total_hrg' => $item['price'] * $item['qty']
+        //         );
+        //         $qq = $item['id'];
+        //         $q = $this->db->query("SELECT * FROM tbl_barang WHERE id_barang='$qq'")->result();
+        //         foreach ($q as $it) {
+        //             $ids = $item['id'];
+        //             $stok_awal = $it->stok_real;
+        //             $stok_akhir = $stok_awal - $item['qty'];
+        //             $this->db->query("UPDATE tbl_barang SET stok_real='$stok_akhir' WHERE id_barang='$ids'");
+        //         }
+        //         $this->m_kasir->input_data($data_detail, 'tbl_transaksidetail');
+        //     }
+        // }
+        // $this->cart->destroy();
+        // redirect('kasir/sukses');
     }
 }
--- application/views/kasir/check_out.php
+++ application/views/kasir/check_out.php
@@ -118,21 +118,103 @@ if (!isset($data)) {
                     </div>
                 </div>
             </div>
-            <div class="col-7 offset-1">
-                <div class="row">
-                    <div class="col-md-12">
-                        <?php
-                        $grand_total = 0;
-                        if ($cart = $this->cart->contents()) {
-                            foreach ($cart as $item) {
-                                $grand_total = $grand_total + $item['subtotal'];
-                            }
-                            echo "<h4>Total Belanja: Rp." . number_format($grand_total, 0, ",", ".") . "</h4>";
-                            ?>
-                            <center>
-                                <h2>Konfirmasi Checkout</h2>
-                            </center>
-                            <form class="form-horizontal" action="<?php echo base_url() ?>kasir/proses_transaksi" method="post" name="frmCO" id="frmCO">
+            <div class="col-8">
+                <?php
+                $grand_total = 0;
+                if ($cart = $this->cart->contents()) {
+                    foreach ($cart as $item) {
+                        $grand_total = $grand_total + $item['subtotal'];
+                        $id_pelanggan = $item['pelanggan'];
+                    }
+                    echo "<center><h4>Total Belanja: Rp." . number_format($grand_total, 0, ",", ".") . "</h4></center><br>";
+                    if ($id_pelanggan != "kosong") {
+                        ?>
+                        <div class="row">
+                            <div class="col-md-6">
+
+                                <center>
+                                    <h2>Pembayaran</h2>
+                                </center>
+
+                                <form class="form-horizontal" action="<?php echo base_url() ?>kasir/proses_transaksi" method="post" name="frmCO" id="frmCO">
+                                    <div class="form-group  has-success has-feedback">
+                                        <label class="control-label col-xs-3" for="lastName">Bayar :</label>
+                                        <div class="col-xs-9">
+                                            <input type="text" class="form-control" name="bayar" id="alamat" placeholder="Pembayaran">
+                                        </div>
+                                    </div>
+                                    <div class="form-group  has-success has-feedback">
+                                        <label class="control-label col-xs-3" for="lastName">Kembalian :</label>
+                                        <div class="col-xs-9">
+                                            <input type="text" class="form-control" name="kembalian" id="alamat" readonly>
+                                        </div>
+                                    </div>
+
+                            </div>
+                            <div class="col-md-6">
+                                <h2>Biodata Pelanggan</h2>
+                                <input type="text" class="form-control" name="id_pelanggan" id="email" placeholder="Email" value="asd">
+                                <input type="hidden" class="form-control" name="id_transaksi" id="email" placeholder="Email" value="<?= $transaksi ?>">
+                                <input name="id_pegawai" type="hidden" value="<?php echo $this->session->userdata("pegawai"); ?>">
+                                <div class="form-group  has-success has-feedback">
+                                    <label class="control-label col-xs-3" for="inputEmail">Nama :</label>
+                                    <div class="col-xs-9">
+                                        <input type="text" class="form-control" name="nama" id="email" placeholder="Nama" value="<?= $data_pelanggan[0]['nama_pelanggan'] ?>" readonly>
+                                    </div>
+                                </div>
+                                <div class="form-group  has-success has-feedback">
+                                    <label class="control-label col-xs-3" for="firstName">Email :</label>
+                                    <div class="col-xs-9">
+                                        <input type="text" class="form-control" name="email" id="nama" placeholder="Email Lengkap" value="<?= $data_pelanggan[0]['email'] ?>" readonly>
+                                    </div>
+                                </div>
+                                <div class="form-group  has-success has-feedback">
+                                    <label class="control-label col-xs-3" for="lastName">Alamat :</label>
+                                    <div class="col-xs-9">
+                                        <input type="text" class="form-control" name="alamat" id="alamat" placeholder="Alamat" value="<?= $data_pelanggan[0]['alamat'] ?>" readonly>
+                                    </div>
+                                </div>
+                                <div class="form-group  has-success has-feedback">
+                                    <label class="control-label col-xs-3" for="phoneNumber">Telp :</label>
+                                    <div class="col-xs-9">
+                                        <input type="tel" class="form-control" name="telp" id="telp" placeholder="No Telp" value="<?= $data_pelanggan[0]['no_hp'] ?>" readonly>
+                                    </div>
+                                </div>
+
+                                <div class="form-group  has-success has-feedback">
+                                    <div class="col-xs-offset-3 col-xs-9">
+                                        <button type="submit" class="btn btn-primary">Simpan</button>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+
+                        </form>
+                    <?php
+                } else {
+
+                    ?>
+                        <div class="row">
+                            <div class="col-md-5">
+                                <center>
+                                    <h2>Pembayaran</h2>
+                                </center>
+                                <form class="form-horizontal" action="<?php echo base_url() ?>kasir/proses_transaksi" method="post" name="frmCO" id="frmCO">
+                                    <div class="form-group  has-success has-feedback">
+                                        <label class="control-label col-xs-3" for="lastName">Bayar :</label>
+                                        <div class="col-xs-9">
+                                            <input type="text" class="form-control" name="bayar" id="alamat" placeholder="Pembayaran">
+                                        </div>
+                                    </div>
+                                    <div class="form-group  has-success has-feedback">
+                                        <label class="control-label col-xs-3" for="lastName">Kembalian :</label>
+                                        <div class="col-xs-9">
+                                            <input type="text" class="form-control" name="kembalian" id="alamat" readonly>
+                                        </div>
+                                    </div>
+                            </div>
+                            <div class="col-md-7">
+                                <h2>Biodata Pelanggan</h2>
                                 <input type="hidden" class="form-control" name="id_pelanggan" id="email" placeholder="Email" value="<?= $pelanggan ?>">
                                 <input type="hidden" class="form-control" name="id_transaksi" id="email" placeholder="Email" value="<?= $transaksi ?>">
                                 <input name="id_pegawai" type="hidden" value="<?php echo $this->session->userdata("pegawai"); ?>">
@@ -166,22 +248,26 @@ if (!isset($data)) {
                                         <button type="submit" class="btn btn-primary">Simpan</button>
                                     </div>
                                 </div>
-                            </form>
-                        <?php
-                    } else {
-                        echo "<h5>Shopping Cart masih kosong</h5>";
-                    }
-                    ?>
-                    </div>
-                </div>
+                            </div>
+                        </div>
+                        </form>
+                    <?php } ?>
+                <?php
+
+            } else {
+                echo "<h5>Shopping Cart masih kosong</h5>";
+            }
+            ?>
             </div>
         </div>
+    </div>
+    </div>
 
-        <!-- Optional JavaScript -->
-        <!-- jQuery first, then Popper.js, then Bootstrap JS -->
-        <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
-        <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
-        <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
 </body>
 
 </html>
